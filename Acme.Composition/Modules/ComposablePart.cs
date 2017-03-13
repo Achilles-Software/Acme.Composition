4d35e6efec5bce2c4d0333280b9c178ff6560147
Todd Thomson <Todd Thomson>
2017-03-13 08:13:43 -0700

    Merged PR 1: Migration to vs 2017 and ASP.NET Core 1.1
    Migration to vs 2017
    Using ASP.NET Core 1.1

---
+++ Acme.Composition/Modules/ComposablePart.cs
@@ -0,0 +1,50 @@
+﻿#region Copyright Notice
+
+// Copyright (c) by Achilles Software, All rights reserved.
+//
+// Licensed under the MIT License. See License.txt in the project root for license information.
+//
+// Send questions regarding this copyright notice to: mailto:Todd.Thomson@achilles-software.com
+
+#endregion
+
+#region Namespaces
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+#endregion
+
+namespace Achilles.Acme.Composition.Modules
+{
+    public class ComposablePart
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="ComposablePart"/>.
+        /// </summary>
+        /// <param name="assembly">The composable part assembly.</param>
+        public ComposablePart( Assembly assembly )
+        {
+            if ( assembly == null )
+            {
+                throw new ArgumentNullException( nameof( assembly ) );
+            }
+
+            Assembly = assembly;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Assembly"/> of the <see cref="ComposablePart"/>.
+        /// </summary>
+        public Assembly Assembly { get; }
+
+        /// <summary>
+        /// Gets the name of the <see cref="ComposablePart"/>.
+        /// </summary>
+        public string Name => Assembly.GetName().Name;
+
+        /// <inheritdoc />
+        public IEnumerable<TypeInfo> Types => Assembly.DefinedTypes;
+    }
+}
---
+++ Acme.Composition/Modules/IModule.cs
@@ -0,0 +1,28 @@
+﻿#region Copyright Notice
+
+// Copyright (c) by Achilles Software, All rights reserved.
+//
+// Licensed under the MIT License. See License.txt in the project root for license information.
+//
+// Send questions regarding this copyright notice to: mailto:Todd.Thomson@achilles-software.com
+
+#endregion
+
+#region Namespaces
+
+using Microsoft.Extensions.DependencyInjection;
+
+#endregion
+
+namespace Achilles.Acme.Composition.Modules
+{
+    /// <summary>
+    /// The interface for modules deployed in an ACME web application.
+    /// </summary>
+    public interface IModule {
+        /// <summary>
+        /// The module initialization method.
+        /// </summary>
+        void Initialize( IServiceCollection services );
+    }
+}
